7e568303eeba0f2cde37bdb6ec6a38e5ea170980
Oliver Jaksch <ojaksch@com-in.de>
2022-09-14 16:46:04 +0200

    ln -sf

--- update_tty2rpi_script.sh
+++ update_tty2rpi_script.sh
@@ -17,6 +17,6 @@ sudo chown -R $(id -un 1000): $(getent passwd "1000" | awk -F ":" '{print $6}')
 sudo chown root: /etc/X11/xorg.conf.d/10-monitor.conf
 sudo chown root: /etc/wpa_supplicant/wpa_supplicant.conf.example
 sudo chown root: /etc/rc.local
-sudo ln ~/update_tty2rpi.sh /usr/local/bin/
+sudo ln -sf ~/update_tty2rpi.sh /usr/local/bin/
 
 [ -z "${SSH_TTY}" ] && echo -e "${fgreen}Press any key to continue\n${freset}"
