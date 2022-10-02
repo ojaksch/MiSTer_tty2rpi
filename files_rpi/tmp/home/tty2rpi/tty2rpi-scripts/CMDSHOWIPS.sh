e82bbc447a060cbab43aee78191ae27d4d40a723
Oliver Jaksch <ojaksch@com-in.de>
2022-10-02 12:09:41 +0200

    CMDSHOWIPS: show hostname also

--- files_rpi/tmp/home/tty2rpi/tty2rpi-scripts/CMDSHOWIPS.sh
+++ files_rpi/tmp/home/tty2rpi/tty2rpi-scripts/CMDSHOWIPS.sh
@@ -3,7 +3,7 @@
 source ~/tty2rpi.ini
 source ~/tty2rpi-user.ini
 
-echo "Your IP adress(es):" > /tmp/ip-adresses
+echo -e "Your IP adress(es) for\nhostname $(hostname -f):\n" > /tmp/ip-adresses
 ip -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $2" "$4}' >> /tmp/ip-adresses
 convert -size ${RESOLUTION} xc:black -pointsize $((${WIDTH}/20)) -fill white -gravity center -draw "text 0,0 '$(cat /tmp/ip-adresses)'" /tmp/ip-adresses.png
 feh -q -F /tmp/ip-adresses.png &
