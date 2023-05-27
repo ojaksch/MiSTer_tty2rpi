431ff9f6541e5a9901849151b8483e853ab96acb
Oliver Jaksch <ojaksch@com-in.de>
2023-05-27 11:07:06 +0200

    Smaller font for IPs because of the length of public IPv6 adresses

--- files_rpi/tmp/home/tty2rpi/tty2rpi-scripts/CMDSHOWIPS.sh
+++ files_rpi/tmp/home/tty2rpi/tty2rpi-scripts/CMDSHOWIPS.sh
@@ -10,5 +10,5 @@ KILLPID feh
 
 echo -e "Your IP adress(es) for\nhostname $(hostname -f):\n" > /tmp/ip-adresses
 ip -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $2" "$4}' >> /tmp/ip-adresses
-convert -size ${RESOLUTION} xc:black -pointsize $((${WIDTH}/20)) -fill white -gravity center -draw "text 0,0 '$(cat /tmp/ip-adresses)'" /tmp/ip-adresses.png
+convert -size ${RESOLUTION} xc:black -pointsize $((${WIDTH}/25)) -fill white -gravity center -draw "text 0,0 '$(cat /tmp/ip-adresses)'" /tmp/ip-adresses.png
 feh --quiet --fullscreen /tmp/ip-adresses.png
