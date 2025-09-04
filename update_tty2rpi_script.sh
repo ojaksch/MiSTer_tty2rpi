f87f425c502b9ca1f60e3fdedc5f640905185534
Larry <github-ojaksch@com-in.de>
2025-09-04 14:26:57 +0200

    Use git from testing branch

--- update_tty2rpi_script.sh
+++ update_tty2rpi_script.sh
@@ -14,7 +14,7 @@ echo "Updating tty2rpi..."
 ! [ -d ${LOCALGITDIR} ] && mkdir ${LOCALGITDIR}
 git -C ${LOCALGITDIR} fetch origin/testing --quiet > /dev/null 2>&1
 if [ ${?} -gt 0 ]; then
-  git -C ${LOCALGITDIR} clone --quiet https://github.com/ojaksch/MiSTer_tty2rpi-testing
+  git -C ${LOCALGITDIR} clone --quiet https://github.com/ojaksch/MiSTer_tty2rpi
   mv ${LOCALGITDIR}/MiSTer_tty2rpi/{.,}* ${LOCALGITDIR}/ > /dev/null 2>&1
   rm -rf ${LOCALGITDIR}/MiSTer_tty2rpi/
 else
