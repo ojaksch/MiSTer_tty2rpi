7aa9f332fe05e34dfbf96e1d340f947971492119
Larry <github-ojaksch@com-in.de>
2025-09-08 10:44:40 +0200

    Typos fixed

--- files_rpi/tmp/home/tty2rpi/tty2rpi-scripts/CMDSHOWIPS.sh
+++ files_rpi/tmp/home/tty2rpi/tty2rpi-scripts/CMDSHOWIPS.sh
@@ -11,9 +11,9 @@ until [ $(ip link | grep -c "state UP") -gt 0 ]; do true; sleep 0.2; done
 DEFGW="$(ip route | head -n1 | grep "default via" | awk '{print $3}')"
 until ping -c1 "${DEFGW}" >/dev/null 2>&1; do sleep 1; done
 
-echo -e "Your IP address(es) for\nhostname $(hostname -f):\n" > /tmp/ip-adresses
-ip -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $2" "$4}' >> /tmp/ip-adresses
-sed -i '/ fd23:/d' /tmp/ip-adresses
-sed -i '/ fe80:/d' /tmp/ip-adresses
-${IMconvert} -size ${RESOLUTION} xc:black -pointsize $((${WIDTH}/25)) -fill white -gravity center -draw "text 0,0 '$(</tmp/ip-adresses)'" /dev/shm/pictmp.png
+echo -e "Your IP address(es) for\nhostname $(hostname -f):\n" > /tmp/ip-addresses
+ip -o addr | awk '!/^[0-9]*: ?lo|link\/ether/ {gsub("/", " "); print $2" "$4}' >> /tmp/ip-addresses
+sed -i '/ fd23:/d' /tmp/ip-addresses
+sed -i '/ fe80:/d' /tmp/ip-addresses
+${IMconvert} -size ${RESOLUTION} xc:black -pointsize $((${WIDTH}/25)) -fill white -gravity center -draw "text 0,0 '$(</tmp/ip-addresses)'" /dev/shm/pictmp.png
 mv /dev/shm/pictmp.png /dev/shm/pic.png
