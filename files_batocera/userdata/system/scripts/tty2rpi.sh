3f8accfe21bf22c0afb4cd0bed6d0853c494bee6
Larry <github-ojaksch@com-in.de>
2025-09-19 10:50:57 +0200

    Fixes for Batocera

--- files_batocera/userdata/system/scripts/tty2rpi.sh
+++ files_batocera/userdata/system/scripts/tty2rpi.sh
@@ -7,7 +7,7 @@ source /userdata/system/scripts/tty2rpi.ini
 case ${1} in
   gameStart)
     CORENAME="${2}"
-    GAMENAME="$(basename ${5})"
+    GAMENAME="$(basename ${5%.*})"
     if [ "${CORENAME}" = "mame" ] && [ "${TTYPICNAME}" = "core" ]; then CORENAME="$(basename ${5%.*})"; fi
     if [ "${CORENAME}" = "mame" ] && [ "${TTYPICNAME}" = "game" ]; then CORENAME="-"; fi
     ;;
@@ -19,7 +19,7 @@ esac
 
 # tty2rpi part
 [ "${TTYPICNAME}" = "core" ] && echo "CMDCOR§-§${CORENAME}" > ${TTYDEV}
-[ "${TTYPICNAME}" = "game" ] && echo "CMDCOR§${CORENAME^^}§${GAMENAME%.*}" > ${TTYDEV}
+[ "${TTYPICNAME}" = "game" ] && echo "CMDCOR§${CORENAME^^}§${GAMENAME}" > ${TTYDEV}
 
 # gameStart gba libretro mgba /userdata/roms/gba/SpaceTwins.gba
 # gameStop gba libretro mgba /userdata/roms/gba/SpaceTwins.gba
