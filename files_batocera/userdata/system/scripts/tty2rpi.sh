183f8f45636f1efaa72dceba5f6b5b688930a31c
Larry <github-ojaksch@com-in.de>
2025-10-07 15:25:43 +0200

    Debug output for Batocera script

--- files_batocera/userdata/system/scripts/tty2rpi.sh
+++ files_batocera/userdata/system/scripts/tty2rpi.sh
@@ -7,7 +7,7 @@ source /userdata/system/scripts/tty2rpi.ini
 case ${1} in
   gameStart)
     CORENAME="${2}"
-    GAMENAME="$(basename ${5%.*})"
+    GAMENAME="$(basename "${5%.*}")"
     if [ "${CORENAME}" = "mame" ] && [ "${TTYPICNAME}" = "core" ]; then CORENAME="$(basename ${5%.*})"; fi
     if [ "${CORENAME}" = "mame" ] && [ "${TTYPICNAME}" = "game" ]; then CORENAME="-"; fi
     ;;
