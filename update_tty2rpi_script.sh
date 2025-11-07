e48df6299722033ab486fc2f14e45420dca8cb5e
Larry <github-ojaksch@com-in.de>
2025-11-07 15:11:28 +0100

    Use rsync quiet

--- update_tty2rpi_script.sh
+++ update_tty2rpi_script.sh
@@ -55,9 +55,11 @@ fi
 [[ -d /tmp/tty2rpi-info ]] || mkdir -p /tmp/tty2rpi-info/tty2rpi
 cp /usr/lib/os-release /tmp/tty2rpi-info/
 cp "${TMPDIR}/tmp/tty2rpi.socket" /tmp/tty2rpi-info/
+cp "${LOCALGITDIR}/.git/HEAD" /tmp/tty2rpi-info/
+cp "${LOCALGITDIR}/.git//FETCH_HEAD" /tmp/tty2rpi-info/
 ip a > /tmp/tty2rpi-info/network
 [ -e /usr/local/bin/showrpimodel ] && cat /proc/cpuinfo | grep Model | cut -d ":" -f 2 > /tmp/tty2rpi-info/pi-model
-rsync -rv --include="*/" --include=".bashrc" --include=".bash_profile" --include=".xinitrc*" --include="*.ini" --include="last_update" --include=".config/**" --include="tty2rpi-scripts/**"    --exclude="*" ~/ /tmp/tty2rpi-info/tty2rpi/
+rsync -qr --include="*/" --include=".bashrc" --include=".bash_profile" --include=".xinitrc*" --include="*.ini" --include="last_update" --include=".config/**" --include="tty2rpi-scripts/**"    --exclude="*" ~/ /tmp/tty2rpi-info/tty2rpi/
 tar -C /tmp/tty2rpi-info/ --zstd -cvf "${TMPDIR}/tmp/tty2rpi-backup.tar.zst" .
 
 systemctl --user daemon-reload
