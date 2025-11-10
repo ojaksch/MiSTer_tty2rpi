69eff4dfb126fe1dabc363f45975ee549c09050a
Larry <github-ojaksch@com-in.de>
2025-11-10 10:13:49 +0100

    Backup is now an external command

---
+++ files_mister/tty2rpi-backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+. /media/fat/tty2rpi/tty2rpi-system.ini
+. /media/fat/tty2rpi/tty2rpi-user.ini
+
+echo "CMDBACKUP" > ${TTYDEV}
--- update_tty2rpi_script.sh
+++ update_tty2rpi_script.sh
@@ -51,17 +51,6 @@ if ! [ -s /etc/systemd/system/splashscreen-shutdown.service ]; then
   sudo systemctl --quiet enable splashscreen-shutdown.service
 fi
 
-# Create Backup
-[[ -d /tmp/tty2rpi-info ]] || mkdir -p /tmp/tty2rpi-info/tty2rpi
-cp /usr/lib/os-release /tmp/tty2rpi-info/
-cp "${TMPDIR}/tmp/tty2rpi.socket" /tmp/tty2rpi-info/
-cp "${LOCALGITDIR}/.git/HEAD" /tmp/tty2rpi-info/
-cp "${LOCALGITDIR}/.git//FETCH_HEAD" /tmp/tty2rpi-info/
-ip a > /tmp/tty2rpi-info/network
-[ -e /usr/local/bin/showrpimodel ] && cat /proc/cpuinfo | grep Model | cut -d ":" -f 2 > /tmp/tty2rpi-info/pi-model
-rsync -qr --include="*/" --include=".bashrc" --include=".bash_profile" --include=".xinitrc*" --include="*.ini" --include="last_update" --include=".config/**" --include="tty2rpi-scripts/**"    --exclude="*" ~/ /tmp/tty2rpi-info/tty2rpi/
-tar -C /tmp/tty2rpi-info/ --zstd -cf "${TMPDIR}/tmp/tty2rpi-backup.tar.zst" .
-
 systemctl --user daemon-reload
 kill -SIGCONT $(pidof inotifywait)
 mv "${TMPDIR}/tmp/pictmp.png" "${TMPDIR}/pic.png"
