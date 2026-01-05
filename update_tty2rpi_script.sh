b4e094501543811ff43c869cc13b4a56b982458d
Larry <github-ojaksch@com-in.de>
2026-01-05 10:05:33 +0100

    Fix switching between repositories

--- update_tty2rpi_script.sh
+++ update_tty2rpi_script.sh
@@ -23,13 +23,13 @@ if [ ${?} -gt 0 ]; then
   git -C ${LOCALGITDIR} clone --quiet https://github.com/ojaksch/MiSTer_tty2rpi
   mv ${LOCALGITDIR}/MiSTer_tty2rpi/{.,}* ${LOCALGITDIR}/ > /dev/null 2>&1
   rm -rf ${LOCALGITDIR}/MiSTer_tty2rpi/
-  [ "${REPOSITORY_URL##*/}" = "main" ] && git -C ${LOCALGITDIR} checkout origin/main
-  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout origin/testing
+  [ "${REPOSITORY_URL##*/}" = "main" ] && git -C ${LOCALGITDIR} checkout main
+  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout testing
 else
   git -C ${LOCALGITDIR} reset --hard origin/main --quiet > /dev/null 2>&1
   git -C ${LOCALGITDIR} clean -xdf --quiet 2>&1
-  [ "${REPOSITORY_URL##*/}" = "main" ] && git -C ${LOCALGITDIR} checkout origin/main
-  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout origin/testing
+  [ "${REPOSITORY_URL##*/}" = "main" ] && git -C ${LOCALGITDIR} checkout main
+  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout testing
 fi
 ! [ -f ~/.xinitrc-extra ] && touch ~/.xinitrc-extra
 TTY2RPIUSER="$(id -un 1000)"
