1472b9ec906cf23d9df299d77b713befdba3023b
Larry <github-ojaksch@com-in.de>
2026-01-05 10:07:31 +0100

    Fix switching between repositories

--- update_tty2rpi_script.sh
+++ update_tty2rpi_script.sh
@@ -24,12 +24,12 @@ if [ ${?} -gt 0 ]; then
   mv ${LOCALGITDIR}/MiSTer_tty2rpi/{.,}* ${LOCALGITDIR}/ > /dev/null 2>&1
   rm -rf ${LOCALGITDIR}/MiSTer_tty2rpi/
   [ "${REPOSITORY_URL##*/}" = "main" ] && git -C ${LOCALGITDIR} checkout main
-  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout testing
+  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout origin/testing
 else
   git -C ${LOCALGITDIR} reset --hard origin/main --quiet > /dev/null 2>&1
   git -C ${LOCALGITDIR} clean -xdf --quiet 2>&1
   [ "${REPOSITORY_URL##*/}" = "main" ] && git -C ${LOCALGITDIR} checkout main
-  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout testing
+  [ "${REPOSITORY_URL##*/}" = "testing" ] && git -C ${LOCALGITDIR} checkout origin/testing
 fi
 ! [ -f ~/.xinitrc-extra ] && touch ~/.xinitrc-extra
 TTY2RPIUSER="$(id -un 1000)"
